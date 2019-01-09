016d1514861e1812768f7454a5be64626967be2f
MaxouMask <hello@maxoumask.com>
2019-01-09 16:14:25 +0100

    added a config to the list for testing purpose

--- src/main.py
+++ src/main.py
@@ -13,13 +13,25 @@ configs = [Config(
     n_step=10,
     memory_capacity=1000,
     optimizer_lr=0.001
-)]
+),
+    Config(
+        nb_epoch=1,
+        with_crop=True,
+        with_color_pre=True,
+        image_size='80x80',
+        nb_games=3,
+        softmax_temp=0.5,
+        n_step=15,
+        memory_capacity=1000,
+        optimizer_lr=0.001
+    )
+]
 
 alien_gym = AlienGym()
 run_number = 0
 for config in configs:
-    cdqn_logger.info('---------Starting run {}---------'.format(run_number))
     for i in range(0, nb_runs):
+        cdqn_logger.info('---------Starting run {}---------'.format(run_number))
         result = alien_gym.run(config=config, run_number=run_number)
         print(result)
         run_number += 1
